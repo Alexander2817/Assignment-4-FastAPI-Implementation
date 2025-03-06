a80700c74416d68fb23f6b26ebea9b0346b92294
Alexander2817 <alande10@charlotte.edu>
2025-03-06 17:31:36 -0500

    feat: fixed removing .venv

--- .venv/main.py
+++ .venv/main.py
@@ -2,7 +2,7 @@ from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
-#Set up for code
+#Set up the code for the assignment
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
